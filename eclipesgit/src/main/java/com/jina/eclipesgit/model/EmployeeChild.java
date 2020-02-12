de3890f4e6603bb7e9a466b845ceac4f6afc2b12
jinesh <jineshbehera06@gmail.com>
2020-02-13 00:25:38 +0530

    child class added

---
+++ eclipesgit/src/main/java/com/jina/eclipesgit/model/EmployeeChild.java
@@ -0,0 +1,33 @@
+package com.jina.eclipesgit.model;
+
+public class EmployeeChild {
+
+	private String childName;
+	private int childAge;
+	private String parentId;
+	
+	
+	public EmployeeChild() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getChildName() {
+		return childName;
+	}
+	public void setChildName(String childName) {
+		this.childName = childName;
+	}
+	public int getChildAge() {
+		return childAge;
+	}
+	public void setChildAge(int childAge) {
+		this.childAge = childAge;
+	}
+	public String getParentId() {
+		return parentId;
+	}
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+	
+}
